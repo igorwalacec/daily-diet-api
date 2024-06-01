44e6dc9097c74c867c12ce74ae78197c6fcbf9fa
Igor Walace Cuevas <icuevas@bankly.com.br>
2024-06-01 20:36:51 -0300

    chore: implement environment to configure database

---
+++ src/env/index.ts
@@ -0,0 +1,24 @@
+import { config } from 'dotenv'
+import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
+  DATABASE_URL: z.string(),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  PORT: z.coerce.number().default(3000),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Environment validation error:', _env.error.format())
+
+  throw new Error('Environment validation error')
+}
+
+export const env = _env.data
