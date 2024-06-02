410d51f80310c2b2b5d54e95498674a5882b2572
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2024-06-02 04:11:43 -0300

    test: add assert to validate cookie on create user

--- test/users.spec.ts
+++ test/users.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, beforeEach, describe, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import { app } from '../src/app'
 import { execSync } from 'child_process'
 import request from 'supertest'
@@ -11,17 +11,23 @@ describe('user routes', () => {
     await app.close()
   })
 
-  beforeEach(() => {
+  beforeEach(async () => {
     execSync('npm run knex migrate:rollback --all')
     execSync('npm run knex migrate:latest')
   })
 
   it('should be create a new user', async () => {
-    await request(app.server)
+    const response = await request(app.server)
       .post('/users')
       .send({
         name: 'User test',
       })
       .expect(201)
+
+    const cookies = response.get('Set-Cookie')
+
+    expect(cookies).toEqual(
+      expect.arrayContaining([expect.stringContaining('userId')]),
+    )
   })
 })
